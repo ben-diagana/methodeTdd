239c07b3b49433fcdfa93e5ee4e9c332d71538a8
diagana youssouf <diaganayoussouf86@gmail.com>
2021-03-26 11:51:18 +0100

    2eme mise a jour

---
+++ fruits.js
@@ -0,0 +1,19 @@
+2 + 2
+10 / 5
+"hello" + "world"
+console.log("Hi")
+let age = 30
+age * 2
+const add3 = (num) =>{
+return num + 3;
+}
+add3
+add3(10)
+"The answer to life the universe and everything is 32"
+"The answer to life the universe and everything is 32"
+// >
+// Math.sqrt
+// Math.sqrt
+// Math
+// Math.
+// Math.fruits = ['banana', 'apple', 'margo"];
---
+++ index.test.js
@@ -0,0 +1,9 @@
+const Todos = require('./index');
+const assert = require('assert').strict;
+
+describe("integration test", function() {
+});
+describe("integration test", function() {
+    it("should be able to add and complete TODOs", function() {
+    });
+});
